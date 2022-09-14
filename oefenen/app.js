a75ad06a66e9730295aafa606e5586b7696dd485
HavinKarakaya <9019924@student.zadkine.nl>
2022-09-14 21:57:06 +0200

    js oef3.1

---
+++ oefenen/app.js
@@ -0,0 +1,56 @@
+//Oefening 1
+
+//String
+let myName  = "Havin";
+//number
+let myAge = 16
+//Boolean
+let diploma = true;
+//Undefined
+let passed =
+
+
+document.write(myName);
+document.write("<br>");
+document.write(myAge);
+document.write("<br>");
+document.write(diploma);
+document.write("<br>");
+document.write(passed);
+document.write("<br>");
+
+/*
+De 7 Operators zijn +optellen, -aftrekken, *vermenigvuldigen, /delen, ++ verhoging met 1,
+-- verlaging met 1, % modulus rest na deling 
+*/
+
+
+//Oefening 2
+
+let num1 = 20
+let num2 = 3
+ 
+//vermenigvuldigen
+let answer = num1 * num2
+document.write(answer);
+document.write("<br>");
+
+//optellen
+let answer2 = num1 + num2
+document.write(answer2);
+document.write("<br>");
+
+//aftrekken
+let answer3 = num1 - num2 
+document.write(answer3);
+document.write("<br>");
+
+//delen
+let answer4 = num1 / num2
+document.write(answer4);
+document.write("<br>");
+
+//modulo
+let answer5 = num1 % num2
+document.write(answer5);
+document.write("<br>");
