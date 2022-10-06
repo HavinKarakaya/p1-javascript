3d5b6f108ab8ae16106977607e41e58fa3658821
HavinKarakaya <9019924@student.zadkine.nl>
2022-10-06 15:08:16 +0200

    opdracht_6.4

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,18 @@
+function calculate(num, num2){
+let answer = num + num2;
+console.log(answer)
+
+}
+calculate(2345, 2);
+
+function multiplyByFive(num){
+let answer = num * 5;
+console.log(answer);
+}
+multiplyByFive(2);
+
+function calculateMinutes(num){
+    let answer = num / 60
+    console.log(answer);
+}
+calculateMinutes(360)
